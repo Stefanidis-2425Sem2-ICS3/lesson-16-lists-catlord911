c29551e19268ec4f1c2af477cf76cc866beae04a
catlord911 <burkelll@students.dsbn.org>
2025-03-26 12:48:08 -0400

    ok so i might notve used a list but uhh

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-
+import random
+howManyDo = input("how many numbers do i generste")
+total = 0
+for i in range(howManyDo):
+    lastRoll = random.randint(0, 100)
+    print(lastRoll)
+    total = total + lastRoll
+total = total/howManyDo
+print("average is", howManyDo)
